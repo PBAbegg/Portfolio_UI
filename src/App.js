af9453cc5c24a05df691fdfd65d42c89b74908d9
Parker Abegg <pbabegg@gmail.com>
2019-10-07 21:03:09 -0600

    added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className="Head-shot" src="https://live.staticflickr.com/65535/48796190808_1036eabaa1_c.jpg" alt="head-shot" />
+
       </header>
     </div>
   );
