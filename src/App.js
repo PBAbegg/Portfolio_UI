0fdc2ae45d1c222e781e381e87c368990dd9bae4
Parker Abegg <pbabegg@gmail.com>
2020-01-28 14:05:52 -0700

    Elimiatied Each child in a list should have a unique key prop error.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Nav } from 'reactstrap';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-
-import Home from './Components/Home'
-import Projects from './Components/Projects/DisplayProject'
-////////////////////////////////////////////ProjectPage
-import Skills from './Components/Skills'
-import WorkExperience from './Components/WorkExperience'
-import Education from './Components/Education'
-import KnowledgeCatalog from './Components/KnowledgeCatalog'
-import References from './Components/References'
-
-
+//import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-    <div>
-          <Nav vertical>      
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/skills">Skills</Link>
-            <Link to="/work_experience">Work Experience</Link>
-            <Link to="/education">Education</Link>
-            <Link to="/knowledge_catalog">Knowledge Catalog</Link>
-            <Link to="/references">References</Link>
-          </Nav>
-        <hr />
+    <div className="App">
+      <header className="App-header">
+      <img className="Head-shot" src="https://live.staticflickr.com/65535/48796190808_1036eabaa1_c.jpg" alt="head-shot" />
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/work_experience">
-          <WorkExperience />
-        </Route>
-        <Route path="/education">
-          <Education />
-        </Route>
-        <Route path="/knowledge_catalog">
-          <KnowledgeCatalog />
-        </Route>
-        <Route path="/references">
-          <References />
-        </Route>
-      </Switch>
+      </header>
     </div>
-    </Router>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import Projects from './Components/Projects/AddNewProject';
-import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const routing = (
-    <Router>>
-        <div>
-            <Route path="/" component={App}/>
-            <Route path="/projects" component={Projects}/>
-        </div>
-    </Router>
-)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
